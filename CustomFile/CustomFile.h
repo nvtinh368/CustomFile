05e116577be083fa02f1b32af94f8bd48f036665
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-23 17:08:39 +0700

    Initial Commit

---
+++ CustomFile/CustomFile.h
@@ -0,0 +1,18 @@
+//
+//  CustomFile.h
+//  CustomFile
+//
+//  Created by Nguyen Van Tinh on 4/23/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFile.
+FOUNDATION_EXPORT double CustomFileVersionNumber;
+
+//! Project version string for CustomFile.
+FOUNDATION_EXPORT const unsigned char CustomFileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFile/PublicHeader.h>
+
+
