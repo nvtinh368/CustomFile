46bd7c8862117225ad7534de7fd83c9e014293c2
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-23 17:10:24 +0700

    show

---
+++ CustomFile/CustomFile.swift
@@ -0,0 +1,13 @@
+//
+//  CustomFile.swift
+//  CustomFile
+//
+//  Created by Nguyen Van Tinh on 4/23/21.
+//
+
+import Foundation
+public struct CustomFile{
+    public static func showme(){
+        print("customFile")
+    }
+}
